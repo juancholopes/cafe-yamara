11382ad18130cdd2758a314bf673cceba0e6446d
juancholopes <177896874+juancholopes@users.noreply.github.com>
2025-11-25 21:11:02 -0500

    Actualizar el diseño de la página "Sobre Nosotros" para mejorar la presentación y la experiencia del usuario. Ajustar el componente Button para mejorar su apariencia y responsividad.

--- src/app/(public)/about/page.tsx
+++ src/app/(public)/about/page.tsx
@@ -1,10 +1,14 @@
+
+import Button from "@/src/components/ui/Button";
+
+
 export default function AboutPage() {
   return (
     <section className="mx-0 p-0 flex flex-row items-center justify-center bg-(--primary-color)">
       {/* Contenido */}
-      <div className=" flex flex-col items-center justify-start min-h-screen py-20 px-4 bg-(--primary-color) w-full">
+      <div className=" flex flex-col items-center justify-center min-h-screen p-6 bg-(--primary-color) w-full">
         <h1 className="text-5xl md:text-7xl font-bold mb-8 text-(--secondary-color) drop-shadow-lg">Sobre nosotros</h1>
-      
+        <Button >Conócenos</Button>
       </div>
     </section>
   );
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -3,7 +3,7 @@ export default function Button({ children, onClick }: { children: React.ReactNod
     return (
       <button
         onClick={onClick}
-        className="bg-(--primary-color) text-white py-2 px-4 rounded hover:bg-(--secondary-color) transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 "
+        className="bg-(--primary-color) text-(--secondary-color) py-6 px-8 rounded-2xl hover:bg-(--secondary-color) transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:text-(--primary-color) text-2xl "
       >
         {children}
       </button>
