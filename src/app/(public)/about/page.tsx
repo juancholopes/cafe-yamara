870d4fe20407083bcaada7988ace28e4f34cfb02
juancholopes <177896874+juancholopes@users.noreply.github.com>
2025-11-28 18:17:42 -0500

    Actualizar el diseño de la página "Sobre Nosotros" para mejorar la presentación del contenido y ajustar el tamaño del título.

--- src/app/(public)/about/page.tsx
+++ src/app/(public)/about/page.tsx
@@ -1,14 +1,14 @@
-
 import Button from "@/src/components/ui/Button";
-
 
 export default function AboutPage() {
   return (
     <section className="mx-0 p-0 flex flex-row items-center justify-center bg-(--primary-color)">
       {/* Contenido */}
-      <div className=" flex flex-col items-center justify-center min-h-screen p-6 bg-(--primary-color) w-full">
-        <h1 className="text-5xl md:text-7xl font-bold mb-8 text-(--secondary-color) drop-shadow-lg">Sobre nosotros</h1>
-        {/* <Button >Conócenos</Button> */}
+      <div className=" flex flex-col items-center justify-center min-h-screen p-6 bg-(--primary-color) w-full gap-8">
+        <h1 className="text-5xl md:text-9xl font-bold text-(--secondary-color) drop-shadow-lg">
+          Sobre nosotros
+        </h1>
+        <Button >Conócenos</Button>
       </div>
     </section>
   );
